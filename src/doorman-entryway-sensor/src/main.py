e2b94c5ec9ca0eae917f24a14b3cce613096ef8a
souldzin <souldzin+git@souldzin.com>
2018-02-12 21:03:25 -0600

    setting up python

---
+++ src/doorman-entryway-sensor/src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from doorman.ir_utils.fake_camera import get_snapshot
+
+def main():
+    print("Here we go!")
+    print(get_snapshot())
+
+if __name__ == '__main__':
+    main()
---
+++ src/doorman-ir-utils/setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='doorman-ir-sensor-utils',
+      version='1.0',
+      packages=['doorman.ir_utils'])
