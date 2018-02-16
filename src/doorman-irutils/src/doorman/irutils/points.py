0c29eecf0099dfa23b1ca4407927a0fa0cbe6280
souldzin <souldzin+git@souldzin.com>
2018-02-16 01:41:13 -0600

    starting on entryway sensor implementation

---
+++ src/doorman-irutils/src/doorman/irutils/points.py
@@ -0,0 +1,9 @@
+
+def find_points(frame, min_temp=30):
+    """Find and return key points of (x,y) in an IR frame
+    
+    arguments:
+    frame -- a 64 element array representing an 8x8 matrix of temperature
+    """
+    return []
+
