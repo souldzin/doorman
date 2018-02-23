601e8968bfaf20b5434946a8470e983252b73772
souldzin <souldzin+git@souldzin.com>
2018-02-23 12:14:36 -0600

    working on doorman-room-monitor client + setting up webpack

--- src/doorman-room-monitor/src/client/main.js
+++ src/doorman-room-monitor/src/client/main.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import '../assets/index.html';
+
+// import static files to let webpack deal with them...
+import '../client-assets/index.html';
+import '../client-assets/styles/main.less';
 import '../../client.config.js';
 
 function App() {
     return (
-        <div>
-            <h1>Really... I'm Loaded!</h1>
-            <p>{DOORMAN_CONFIG.API_URL}</p>
+        <div className="home">
+            <div className="home-item big-text">
+                <span>11</span>
+            </div>
+            <div className="home-item">
+                <p>{DOORMAN_CONFIG.API_URL}</p>
+            </div>
         </div>
     );
 }
--- src/doorman-room-monitor/webpack.config.js
+++ src/doorman-room-monitor/webpack.config.js
@@ -1,7 +1,12 @@
-var path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractLess = new ExtractTextPlugin({
+    filename: '[name].css'
+});
 
 module.exports = {
-    entry: "./src/client/main.js",
+    entry: './src/client/main.js',
     output: {
         path: path.resolve(__dirname, 'dist', 'public'),
         filename: 'main.js'
@@ -9,31 +14,46 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /client.config(\..*)?.js$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: "client.config.[ext]"
-                    }
-                }
-            },
-            {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader",
+                loader: 'babel-loader',
                 query: {
                     presets: ['es2015', 'react']
                 }
             },
             {
-                test: /\.(html)$/,
+                test: /(client\.config\.js)|(\.(html)$)/,
                 use: {
                     loader: 'file-loader',
                     options: {
-                        name: "[name].[ext]"
+                        name: '[name].[ext]'
                     }
                 }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: 'fonts',
+                        outputPath: 'fonts',
+                        name: '[name].[hash].[ext]'
+                    }
+                }
+            },
+            {
+                test: /\.less$/,
+                use: extractLess.extract({
+                    use: [{
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'less-loader'
+                    }]
+                })
             }
         ]
-    }
+    },
+    plugins: [
+        extractLess
+    ]
 };
