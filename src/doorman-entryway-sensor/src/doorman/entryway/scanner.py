8b8ac25c59577c0f96fe4935176b16b828f2b35f
souldzin <souldzin+git@souldzin.com>
2018-02-24 14:56:45 +0000

    debugging sensor scanner

--- src/doorman-entryway-sensor/src/doorman/entryway/scanner.py
+++ src/doorman-entryway-sensor/src/doorman/entryway/scanner.py
@@ -1,29 +1,34 @@
 import time
 from rx import Observable
 
-BOUNDARY = (22*64) + 6*20
+BOUNDARY = (22*64) + 60
 
 def get_next_state(state, next_zone):
-    if next_zone == state["zone"]:
-        return {
-            "zone": state["zone"],
-            "time": state["time"],
-            "event": None
-        }
-
+    prev_time = state["time"]
+    prev_zone = state["zone"]
     next_time = time.time()
     event = None
+    entered = state["entered"] if next_zone == "high" else False
 
-    if next_zone == "low":
+    if prev_zone == next_zone:
+        if next_zone == "high" and not entered and next_time > (prev_time + 2):
+            entered = True
+            event = {
+                "type": "enter",
+                "delta": 1
+            }
+        next_time = state["time"]
+    elif next_zone == "low" and next_time < (prev_time + 2):
         event = {
-            "type": "exit" if next_time < state["time"]+ 2 else "enter",
+            "type": "exit",
             "delta": 1
         }
 
     return {
         "zone": next_zone,
         "time": next_time,
-        "event": event
+        "event": event,
+        "entered": entered
     }
 
 def get_zone(frame):
@@ -33,7 +38,8 @@ def get_zone(frame):
 def scan_frames(frames):
     initial_state = {
         "zone": "low",
-        "time_entered": time.time(),
+        "entered": False,
+        "time": time.time(),
         "event": None
     }
 
