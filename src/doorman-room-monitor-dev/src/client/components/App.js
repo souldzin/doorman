be6a7b080b4bc644b14f62609cca4124e67cedaa
souldzin <souldzin+git@souldzin.com>
2018-03-24 07:27:59 -0500

    UC1 add recording feature

--- src/doorman-room-monitor-dev/src/client/components/App.js
+++ src/doorman-room-monitor-dev/src/client/components/App.js
@@ -3,14 +3,17 @@ import React from "react";
 import CountDisplay from "./CountDisplay";
 import {FrameDisplay} from "./Frame";
 import io from "socket.io-client";
+const download = require("downloadjs");
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
+        this._save = [];
         this.state = {
             count: 0,
-            frame: []
+            frame: [],
+            isRecording: false
         };
     }
 
@@ -20,6 +23,33 @@ class App extends React.Component {
 
     componentWillUnmount() {
 
+    }
+
+    toggleRecording() {
+        if(this.state.isRecording) {
+            this.stopRecording();
+        } else {
+            this.startRecording();
+        }
+    }
+
+    stopRecording() {
+        this.setState({
+            isRecording: false
+        });
+        const frames = this._save;
+        this._save = [];
+
+        const data = JSON.stringify({
+            frames
+        });
+        download(data, "recording.json", "application/json");
+    }
+
+    startRecording() {
+        this.setState({
+            isRecording: true
+        });
     }
 
     _connectToSocket() {
@@ -43,29 +73,40 @@ class App extends React.Component {
             this.setState({
                 frame
             });
+
+            if(this.state.isRecording) {
+                this._save.push(frame);
+            }
         });
 
         this._socket = socket;
     }
 
     render() {
-        const {count, frame} = this.state;
+        const {count, frame, isRecording} = this.state;
 
         return (
-            <div className="home">
-                <div className="home-item">
-                    <h1>Occupancy</h1>
-                    <div className="big-text">
-                        <CountDisplay count={count} />
+            <div>
+                <div className="controls">
+                    <button className="btn" onClick={() => this.toggleRecording()}>
+                        {!isRecording ? "Start Recording" : "Stop"}                    
+                    </button>
+                </div>
+                <div className="home">
+                    <div className="home-item">
+                        <h1>Occupancy</h1>
+                        <div className="big-text">
+                            <CountDisplay count={count} />
+                        </div>
                     </div>
-                </div>
-                <div className="home-item">
-                    <h1>Entryway Sensor 1</h1>
-                    <FrameDisplay frame={frame} />
-                </div>
-                <div className="home-item">
-                    <h1>Entryway Sensor 2</h1>
-                    <FrameDisplay />
+                    <div className="home-item">
+                        <h1>Entryway Sensor 1</h1>
+                        <FrameDisplay frame={frame} />
+                    </div>
+                    <div className="home-item">
+                        <h1>Entryway Sensor 2</h1>
+                        <FrameDisplay />
+                    </div>
                 </div>
             </div>
         );
