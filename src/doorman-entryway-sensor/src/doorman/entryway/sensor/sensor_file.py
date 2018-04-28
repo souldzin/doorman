566f6f16d2e1534832bf0e117fd234b1e0b5f58f
souldzin <souldzin+git@souldzin.com>
2018-04-27 23:57:43 -0500

    setup automated tests

--- src/doorman-entryway-sensor/src/doorman/entryway/sensor/sensor_file.py
+++ src/doorman-entryway-sensor/src/doorman/entryway/sensor/sensor_file.py
@@ -8,11 +8,11 @@ class FileSensor:
 
         self._frames = data["frames"]
 
-    def get_frames(self, scheduler):
+    def get_frames(self, scheduler=None, rel_time=120):
         return Observable.generate_with_relative_time(0, 
             lambda x: x < len(self._frames),
             lambda x: x + 1,
             lambda x: self._frames[x],
-            lambda x: 120,
+            lambda x: rel_time,
             scheduler=scheduler
         )
